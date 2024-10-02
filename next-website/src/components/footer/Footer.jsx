a7b05162936882e8fbeeb3511f16902acad6cbc9
Bernardo Alves <bernardoalves330@gmail.com>
2024-10-02 19:03:45 -0300

    added tailwind and changed a lot of stuff

---
+++ next-website/src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div
+            className='h-12 flex items-center justify-between'
+        >
+            ©2023 Lamamia. All rights reserved.
+        </div>
+    )
+}
+
+export default Footer
