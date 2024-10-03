60a08ff0fae4fd40ae28981c6a47cb1fcc1a8e01
Bernardo Alves <bernardoalves330@gmail.com>
2024-10-03 17:02:51 -0300

    portfolio page

--- next-website/src/app/contact/page.jsx
+++ next-website/src/app/contact/page.jsx
@@ -1,7 +1,79 @@
+import Image from "next/image";
+import ButtonLink from "@/components/button/ButtonLink";
+
 const Contact = () => {
     return (
-        <div>Contact</div>
-    )
-}
+        <div>
+            <h1 className="text-6xl mb-14 text-center">Let's Keep in Touch</h1>
+            <div className="flex items-center gap-24">
+                <div className="hidden flex-1 h-[500px] md:block relative">
+                    <Image
+                        src="/contact.png"
+                        alt=""
+                        fill={true}
+                        className='object-contain'
+                    />
+                </div>
+                <form className='flex-1 flex flex-col gap-5'>
+                    <input type="text" placeholder="name" className="p-5 bg-transparent  outline-none text-gray-400 border-2 border-gray-400 text-xl font-bold" />
+                    <input type="text" placeholder="email" className="p-5 bg-transparent outline-none text-gray-400 border-2 border-gray-400 text-xl font-bold" />
+                    <textarea
+                        className="p-5 bg-transparent outline-none text-gray-400 border-2 border-gray-400 text-xl font-bold"
+                        placeholder="message"
+                        cols="20"
+                        rows="10"
+                    ></textarea>
+                    <ButtonLink url="#">Send</ButtonLink>
+                </form>
+            </div>
+        </div>
+    );
+};
 
-export default Contact
+export default Contact;
+
+
+// .content {
+//     display: flex;
+//     align - items: center;
+//     gap: 100px;
+// }
+
+// .imgContainer {
+//     flex: 1;
+//     height: 500px;
+//     position: relative;
+// }
+
+// .image {
+//     object - fit: contain;
+//     /* animation: move 3s infinite ease alternate; */
+// }
+
+// @keyframes move {
+//   from {
+//         transform: translateY(-15px);
+//     }
+//   to {
+//         transform: translateY(0px) scale(1.03);
+//     }
+// }
+
+// .form {
+//     flex: 1;
+//     display: flex;
+//     flex - direction: column;
+//     gap: 20px;
+// }
+
+// .input,
+// .textArea {
+//     padding: 20px;
+//     background - color: transparent;
+//     border: none;
+//     outline: none;
+//     color: #bbb;
+//     border: 3px solid #bbb;
+//     font - size: 20px;
+//     font - weight: bold;
+// }
--- next-website/src/app/portfolio/page.jsx
+++ next-website/src/app/portfolio/page.jsx
@@ -1,7 +1,23 @@
+import React from "react";
+import Link from "next/link";
+
 const Portfolio = () => {
     return (
-        <div>Portfolio</div>
-    )
-}
+        <div>
+            <h1 className="m-5 text-8xl">Choose a gallery</h1>
+            <div className="flex gap-12">
+                <Link href="/portfolio/illustrations" className="border-2 rounded-md w-[300px] h-[400px] relative bg-cover bg-[url('/illustration.png')] group">
+                    <span className="absolute right-2 bottom-2 text-3xl font-bold group-hover:text-green-500 transition-colors ">Illustrations</span>
+                </Link>
+                <Link href="/portfolio/websites" className="border-2 rounded-md w-[300px] h-[400px] relative bg-cover bg-[url('/websites.jpg')] group">
+                    <span className="absolute right-2 bottom-2 text-3xl font-bold group-hover:text-green-500 transition-colors">Websites</span>
+                </Link>
+                <Link href="/portfolio/application" className="border-2 rounded-md w-[300px] h-[400px] relative bg-cover bg-[url('/apps.jpg')] group">
+                    <span className="absolute right-2 bottom-2 text-3xl font-bold group-hover:text-green-500 transition-colors">Application</span>
+                </Link>
+            </div>
+        </div>
+    );
+};
 
-export default Portfolio
+export default Portfolio;
